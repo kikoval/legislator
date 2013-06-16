87d6295705177c4a8b8a0199582b8a768f6ee986
Kristian Valentin <valentin.kristian@gmail.com>
2013-06-16 14:22:33 +0200

    adding missing file

---
+++ src/Legislator/LegislatorBundle/Form/DocumentNewType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Legislator\LegislatorBundle\Form;
+
+use Legislator\LegislatorBundle\Form\ContentSectionType;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DocumentNewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description', 'textarea')
+            ->add('file')
+            ->add('file_substantiation')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Legislator\LegislatorBundle\Entity\Document'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'legislator_document';
+    }
+}
