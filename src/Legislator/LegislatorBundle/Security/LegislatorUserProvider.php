d120abb381908d4bcccfccce89560318c7658d8f
Kristian Valentin <valentin.kristian@gmail.com>
2013-07-14 18:58:49 +0200

    refactoring user provider for cosgin login, full name is now refreshed from LDAP on every login

--- src/Legislator/LegislatorBundle/Security/LegislatorUserProvider.php
+++ src/Legislator/LegislatorBundle/Security/LegislatorUserProvider.php
@@ -30,28 +30,35 @@ class LegislatorUserProvider implements UserProviderInterface {
 		if ($this->cosign_login_enabled) {
 			$ldapSearch = $this->container->get('legislator.teacher_search');
 			$user_info = $ldapSearch->byLogin($username);
+			if (!array_key_exists($username, $user_info)) {
+				throw new UsernameNotFoundException(sprintf('Username "%s" not found in LDAP.', $username));
+			}
 			$user_info = $user_info[$username];
 
 			$org_unit = $this->container->getParameter('org_unit');
 
 			// checking org unit if set
-			if ($org_unit !== null && is_array($user_info)
+			if ($org_unit !== null
 	        		&& array_search($org_unit, $user_info['orgUnits']) === FALSE) {
 	        	throw new AccessDeniedException(sprintf('Username "%s" does not belong to unit "%s".', $username, $org_unit));
 	        }
 
+	        // creating a new user when logging in for the first time
 		    if ($user === null) {
                 $user = $this->userManager->createUser();
                 $user->setUsername($username);
                 $user->setEmail("$username@uniba.sk");
                 $user->setPassword("$username@uniba.sk");
                 $user->setEnabled(1);
-                // set full name from LDAP
-                $user->setFirstName($user_info['givenName']);
-                $user->setSurname($user_info['familyName']);
-
-                $this->userManager->updateUser($user);
 			}
+
+			// set full name from LDAP
+			$user->setFirstName($user_info['givenName']);
+			$user->setSurname($user_info['familyName']);
+
+			$this->userManager->updateUser($user);
+
+
 		} else {
             if (!$user) {
                 throw new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $username));
@@ -72,24 +79,6 @@ class LegislatorUserProvider implements UserProviderInterface {
 
         if (null === $reloadedUser = $this->userManager->findUserBy(array('id' => $user->getId()))) {
             throw new UsernameNotFoundException(sprintf('User with ID "%d" could not be reloaded.', $user->getId()));
-        }
-
-        // filling up full name from LDAP if enabled
-        if ($this->cosign_login_enabled) {
-        	$name = $reloadedUser->getFirstName();
-        	if (empty($name)) {
-        		$username = $reloadedUser->getUsername();
-	        	$ldapSearch = $this->container->get('legislator.teacher_search');
-	        	$user_info = $ldapSearch->byLogin($username);
-	        	if (is_array($user_info)) {
-	        		$user_info = $user_info [$username];
-
-					$reloadedUser->setFirstName($user_info['givenName']);
-					$reloadedUser->setSurname($user_info['familyName']);
-
-					$this->userManager->updateUser($reloadedUser);
-	        	}
-        	}
         }
 
         return $reloadedUser;
