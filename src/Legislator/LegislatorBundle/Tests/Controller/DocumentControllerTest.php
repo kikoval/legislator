eee9961eeeeea2b7fb789c32b0e8e13d4f00150d
Kristian Valentin <kiko.val@gmail.com>
2013-08-04 11:22:02 +0200

    added test for adding new version of a document

--- src/Legislator/LegislatorBundle/Tests/Controller/DocumentControllerTest.php
+++ src/Legislator/LegislatorBundle/Tests/Controller/DocumentControllerTest.php
@@ -2,7 +2,9 @@
 
 namespace Legislator\LegislatorBundle\Tests\Controller;
 
+use Legislator\LegislatorBundle\Entity\Document;
 use Legislator\LegislatorBundle\Tests\MyWebTestCase;
+
 
 class DocumentControllerTest extends MyWebTestCase
 {
@@ -59,6 +61,52 @@ class DocumentControllerTest extends MyWebTestCase
 
 		// there is only one document left
 		$this->assertCount(1, $crawler->filter('tbody tr'));
+
+	}
+
+	public function testNewVersion()
+	{
+		$crawler = $this->client->request('GET', $this->getUrl('legislator_homepage'));
+		$this->assertTrue($this->client->getResponse()->isSuccessful());
+
+		$num_documents = $crawler->filter('tbody tr')->count();
+
+		// go to a document
+		$link = $crawler->filter('a:contains("test document for comments")')->eq(0)->link();
+		$crawler = $this->client->click($link);
+		$this->assertTrue($this->client->getResponse()->isSuccessful());
+
+		// got to edit form
+		$link = $crawler->filter('a.edit')->eq(0)->link();
+		$crawler = $this->client->click($link);
+		$this->assertTrue($this->client->getResponse()->isSuccessful());
+
+		// set status to finished to enable adding new version
+		$form = $crawler->filter('input[type=submit].save')->eq(0)->form();
+		$form['legislator_document[status]']->select(Document::STATUS_FINISHED);
+		$this->client->submit($form);
+		$crawler = $this->client->followRedirect();
+		$this->assertTrue($this->client->getResponse()->isSuccessful());
+		$this->assertCount(1, $crawler->filter('a.add'));
+
+		// go to new version form
+		$link = $crawler->filter('a.add')->eq(0)->link();
+		$crawler = $this->client->click($link);
+		$this->assertTrue($this->client->getResponse()->isSuccessful());
+
+		// add new version
+		$form = $crawler->filter('input[type=submit].add')->eq(0)->form();
+		$temp = tmpfile();  // creates tmp file
+		fwrite($temp, 'new version test file');
+		$meta_data = stream_get_meta_data($temp);
+		$form['legislator_document[file]']->upload($meta_data['uri']);
+		$form['legislator_document[description]'] = '++new version++';
+		fclose($temp);  // deletes tmp file
+		$this->client->submit($form);
+		$crawler = $this->client->followRedirect();
+		$this->assertTrue($this->client->getResponse()->isSuccessful());
+		$this->assertCount($num_documents + 1, $crawler->filter('tbody tr'));
+		$this->assertCount(1, $crawler->filter('td:contains("++new version++")'));
 
 	}
 }
